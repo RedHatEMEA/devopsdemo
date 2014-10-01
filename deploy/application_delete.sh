df16c259c20c37b8474a498ef53e32caba491907
Jim Minter <jminter@redhat.com>
2014-10-01 19:37:40 +0100

    minor fix

--- deploy/application_delete.sh
+++ deploy/application_delete.sh
@@ -19,7 +19,7 @@ delete_stacks() {
 delete_openshift() {
   utils/deploy-openshift.py delete https://$BROKER_IP/broker/rest ticketmonster $PREFIX redhat || true
 
-  echo cn=$PREFIX,ou=Users,dc=demo | ldapdelete -H ldap://dns.demo/ -D 'cn=Manager,dc=demo' -w redhat
+  echo cn=$PREFIX,ou=Users,dc=demo | ldapdelete -H ldap://dns.demo/ -D 'cn=Manager,dc=demo' -w redhat || true
 }
 
 get_ips
